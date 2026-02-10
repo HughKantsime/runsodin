44314b1b429b030cb41a6b3ea4f1298001171c90
hughkantsime <mshane18@gmail.com>
2026-02-10 21:52:32 +0000

    release: v1.0.24

--- backend/main.py
+++ backend/main.py
@@ -99,7 +99,7 @@ _version_file = _pathlib.Path(__file__).parent.parent / "VERSION"
 if _version_file.exists():
     __version__ = _version_file.read_text().strip()
 else:
-    __version__ = "1.0.23"
+    __version__ = "1.0.24"
 
 
 def get_db():
