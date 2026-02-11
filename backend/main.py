fcfcacfe761a2349e4a4e24ce3c3298ff4614a16
root <root@odin.smithlab.local>
2026-02-11 12:35:26 +0000

    release: bump version to 1.0.31

--- backend/main.py
+++ backend/main.py
@@ -99,7 +99,7 @@ _version_file = _pathlib.Path(__file__).parent.parent / "VERSION"
 if _version_file.exists():
     __version__ = _version_file.read_text().strip()
 else:
-    __version__ = "1.0.29"
+    __version__ = "1.0.31"
 
 
 def get_db():
