b1b01ad841da7c0688b981639636ba98bac3e5fc
hughkantsime <mshane18@gmail.com>
2026-02-12 11:38:26 +0000

    release: bump version to 1.1.4

--- backend/main.py
+++ backend/main.py
@@ -100,7 +100,7 @@ _version_file = _pathlib.Path(__file__).parent.parent / "VERSION"
 if _version_file.exists():
     __version__ = _version_file.read_text().strip()
 else:
-    __version__ = "1.1.3"
+    __version__ = "1.1.4"
 
 
 def get_db():
