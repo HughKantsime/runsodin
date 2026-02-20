0829bd03c5ffa4caa2710073780f53941dd605b2
Shane Smith <shanesmith@MacBookPro.localdomain>
2026-02-20 17:12:52 -0500

    release: bump version to 1.3.49

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,6 +1,6 @@
 // O.D.I.N. - Service Worker for Push Notifications
 
-const CACHE_NAME = 'odin-v1.3.48';
+const CACHE_NAME = 'odin-v1.3.49';
 
 // Install event
 self.addEventListener('install', (event) => {
