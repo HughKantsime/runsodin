9b31b3977f73f7ae48709762ef5fd0f36f0fd350
roger-dev <roger-dev@rogerclaw.local>
2026-03-11 23:02:44 +0000

    release: bump version to 1.5.4

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,6 +1,6 @@
 // O.D.I.N. - Service Worker for Push Notifications
 
-const CACHE_NAME = 'odin-v1.5.3';
+const CACHE_NAME = 'odin-v1.5.4';
 
 // Install event
 self.addEventListener('install', (event) => {
