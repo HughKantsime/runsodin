78075387556ccfe5dc9ec884160b7f63973c5b89
HughKantsime <hughkantsime@users.noreply.github.com>
2026-05-01 06:37:55 -0400

    release: bump version to 1.9.6

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,6 +1,6 @@
 // O.D.I.N. - Service Worker for Push Notifications
 
-const CACHE_NAME = 'odin-v1.9.5';
+const CACHE_NAME = 'odin-v1.9.6';
 
 // Install event
 self.addEventListener('install', (event) => {
